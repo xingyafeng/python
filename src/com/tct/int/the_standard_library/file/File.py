fc2b9f05f1208b5f19bf5dd32a56b6f41118be8a
yafeng.xing <yafeng.xing@tcl.com>
2020-07-25 18:37:44 +0800

    test file wildcards.

--- src/com/tct/int/the_standard_library/file/File.py
+++ src/com/tct/int/the_standard_library/file/File.py
@@ -11,10 +11,9 @@ from com.tct.int.tools.Utils import docmd, chdir
 
 if __name__ == '__main__':
 
-    t=''
-    test_path = '/data/nishome/td/yafeng.xing/workspace/project/android'
+    path = '/data/nishome/td/yafeng.xing/workspace/project/android'
 
-    os.chdir(test_path)
+    os.chdir(path)
 
     for oneDir in glob.glob("*"):
         if os.path.isdir(oneDir):
@@ -27,8 +26,9 @@ if __name__ == '__main__':
             mtime = time.time()
             print 'it is %s %s.%d' % (oneDir, oneDir, mtime)
             docmd('mv %s %s.%d' % (oneDir, oneDir, mtime))
-            # docmd('sleep 1000s')
             docmd('rm -vrf %s.%d' % (oneDir, mtime))
             print '----| end'
 
-    print chdir('/home')
+    print chdir('/data/nishome/td/yafeng.xing/jobs')
+
+    print glob.glob('*')
